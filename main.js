3a3960f0d17428b9f8b53ddef0e3243737df2ab8
Kaya <kaya_chen@outlook.com>
2024-08-18 13:39:03 +0800

    update

--- main.js
+++ main.js
@@ -1,67 +1,40 @@
 // 引入其他依賴
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
 
-// 創建一個 script 標籤
-const swiperScript = document.createElement('script');
-swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
+// import styles bundle
+import 'swiper/css/bundle';
 
-// 將 script 標籤添加到 body 的末尾
-document.body.appendChild(swiperScript);
-
-// 等待 Swiper 資源加載完成
-swiperScript.onload = () => {
-  // 初始化 Swiper
-  const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 1, // 一次呈現幾張幻燈片
-    slidesPerGroup: 1, // 每組包含幾張幻燈片
-    spaceBetween: 24, // 幻燈片之間的間距
-    
-    pagination: {
-      el: '.swiper-pagination-custom', // 分頁元素
-      type: 'fraction', // 分頁類型
-    },
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,/*(一次呈現幾張)*/
+  slidesPerGroup: 1,/*(一個群組內有幾張投影片)*/
+  spaceBetween: 24,
+  // ***(分頁),
+  pagination: {
+    el: ".swiper-pagination-custom" /*(pagination)*/,
+    type: "fraction",
+  },
     navigation: {
-      nextEl: '.swiper-next', // 下一張按鈕
-      prevEl: '.swiper-prev', // 上一張按鈕
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev"
     },
 
-    breakpoints: {
-        992: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-          spaceBetween: 48,
-        }
-    }
+  breakpoints: {
+    996: {
+      slidesPerView: 2,/*(一次呈現幾張)*/
+      slidesPerGroup: 2,/*(一個群組內有幾張投影片)*/
+      spaceBetween: 48,
+    },
+  },
+});
 
-  });
-
-  // const swiper2 = new Swiper('.look-swiper', {
-  //   slidesPerView: 1, // 一次呈現幾張幻燈片
-  //   slidesPerGroup: 1, // 每組包含幾張幻燈片
-  //   spaceBetween: 24, // 幻燈片之間的間距
-    
-  //   pagination: {
-  //     el: '.swiper-pagination-custom', // 分頁元素
-  //     type: 'fraction', // 分頁類型
-  //   },
-  //   navigation: {
-  //     nextEl: '.swiper-next', // 下一張按鈕
-  //     prevEl: '.swiper-prev', // 上一張按鈕
-  //   },
-  //   // autoplay: {
-  //   //   delay: 2500,
-  //   //   disableOnInteraction: false,
-  //   // },
-
-  //   breakpoints: {
-  //       992: {
-  //         slidesPerView: 1,
-  //         slidesPerGroup: 1,
-  //         spaceBetween: 48,
-  //       }
-  //   }
-
-  // });
-
-};
+const swiper2 = new Swiper(".lookSwiper", {
+  slidesPerView: 1,/*(一次呈現幾張)*/
+  slidesPerGroup: 1,/*(一個群組內有幾張投影片)*/
+  spaceBetween: 24,
+  autoplay: {
+    delay: 3000,
+  },
+});
